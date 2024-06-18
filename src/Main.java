1580d425a60601a82bed6453839d5e40e1eb9b13
moAmer1468 <mohamed.amer1468@gmail.com>
2024-06-17 23:38:18 -0700

    new Commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+//        System.out.println("Ctrl + D");
+//        System.out.println("This will take a copy of the line down");
+//        System.out.println("=======================");
+//        System.out.println("Ctrl + Alt + Shift + up Arrow");
+//        System.out.println("This will move the line up or down");
+//        System.out.println("============================");
+//        System.out.println("How to format Your code in intellij");
+//        System.out.println("Ctrl + Alt + L ");
+//        System.out.println("===========================");
+//
 
-        System.out.println("Ctrl + D");
-        System.out.println("This will take a copy of the line down");
-        System.out.println("=======================");
-        System.out.println("Ctrl + Alt + Shift + up Arrow");
-        System.out.println("This will move the line up or down");
-        System.out.println("============================");
-        System.out.println("How to format Your code in intellij");
-        System.out.println("Ctrl + Alt + L ");
-        System.out.println("===========================");
-        
+        System.out.println(comparison(90,323));
 
     }
+    public static int comparison(int first,int second){
+        if(first>second){
+            return first;
+        }else
+            return second;
+    }
+
 }
